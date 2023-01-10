bee2749ab073ea505ebec801cf17c16f8e7446c6
胡鵬 <hupgwuty@163.com>
2023-01-10 17:47:39 +0800

    规范常量

--- src/main/java/online/hupeng/quickstransport/QuickTransport.java
+++ src/main/java/online/hupeng/quickstransport/QuickTransport.java
@@ -13,13 +13,14 @@ import net.minecraftforge.fml.event.lifecycle.InterModEnqueueEvent;
 import net.minecraftforge.fml.event.lifecycle.InterModProcessEvent;
 import net.minecraftforge.fml.event.server.FMLServerStartingEvent;
 import net.minecraftforge.fml.javafmlmod.FMLJavaModLoadingContext;
+import online.hupeng.quickstransport.constant.ModConstant;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 
 import java.util.stream.Collectors;
 
 // The value here should match an entry in the META-INF/mods.toml file
-@Mod("quicktransport")
+@Mod(ModConstant.MOD_ID)
 public class QuickTransport
 {
     // Directly reference a log4j logger.
---
+++ src/main/java/online/hupeng/quickstransport/constant/ModConstant.java
@@ -0,0 +1,12 @@
+package online.hupeng.quickstransport.constant;
+
+/**
+ * mod基础常量类
+ */
+public class ModConstant {
+
+    /**
+     * 模组id
+     */
+    public final static String MOD_ID = "quicktransport";
+}
