2d622c5a89aee20fa9c32e64bce1f7932ad4bfc8
胡鵬 <hupgwuty@163.com>
2023-01-12 10:15:42 +0800

    监听用户登出事件保存WorldSaveData

---
+++ src/main/java/online/hupeng/quickstransport/event/listener/PlayerLoggedOutSavingListener.java
@@ -0,0 +1,27 @@
+package online.hupeng.quickstransport.event.listener;
+
+import net.minecraft.entity.player.PlayerEntity;
+import net.minecraft.world.storage.DimensionSavedDataManager;
+import net.minecraftforge.api.distmarker.Dist;
+import net.minecraftforge.event.entity.player.PlayerEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.fml.common.Mod;
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+
+/**
+ * 玩家登出事件监听器
+ */
+@Mod.EventBusSubscriber(Dist.DEDICATED_SERVER)
+public class PlayerLoggedOutSavingListener {
+
+    private final static Logger logger = LogManager.getLogger(PlayerLoggedOutSavingListener.class);
+
+    @SubscribeEvent
+    public static void onPlayerLoggedOutEvent(PlayerEvent.PlayerLoggedOutEvent playerLoggedOutEvent) {
+        PlayerEntity player = playerLoggedOutEvent.getPlayer();
+        logger.info("响应玩家登出事件保存世界存储信息, 玩家uuid: {}, 玩家名称: {}", player.getUUID(), player.getName().getString());
+        DimensionSavedDataManager dimensionSavedDataManager = player.getCommandSenderWorld().getServer().overworld().getDataStorage();
+        dimensionSavedDataManager.save();
+    }
+}
